3cc34f87c588ce530fa39d62c83999d008c190b2
HAMDIhajer <hamdihajer.94@gmail.com>
2023-03-03 07:42:00 -0500

    script

---
+++ file_tree.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+sudo mkdir -p /appli
+sudo chown adm1:adm1 /appli
+mkdir -p /appli/get_file
+mkdir -p /appli/get_file/shl
+mkdir -p /appli/get_file/cfg
+mkdir -p /appli/get_file/log
+mkdir -p /appli/get_file/data
+
